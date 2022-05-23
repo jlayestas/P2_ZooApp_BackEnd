c66e0952bf2bdda9ed64199077e5bee07d35d288
sangar3 <55110746+sangar3@users.noreply.github.com>
2022-05-22 21:43:51 -0500

    initial project commit
    added all dependencies for spring project

---
+++ ZooApp/src/main/java/com/zoo/ZooAppApplication.java
@@ -0,0 +1,13 @@
+package com.zoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZooAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZooAppApplication.class, args);
+	}
+
+}
---
+++ ZooApp/src/test/java/com/zoo/ZooAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZooAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
