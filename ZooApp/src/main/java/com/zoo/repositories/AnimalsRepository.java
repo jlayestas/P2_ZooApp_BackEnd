ae6c75880a58b646110750f77089028b441041d5
jlayestas <93059207+jlayestas@users.noreply.github.com>
2022-05-24 14:00:03 -0600

    Animals Repository
    Will work on testing next.

---
+++ ZooApp/src/main/java/com/zoo/repositories/AnimalsRepository.java
@@ -0,0 +1,20 @@
+package com.revature.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.revature.models.Animals;
+
+@Repository
+@Transactional
+public interface AnimalsRepository extends JpaRepository<Animals, Integer>{
+
+	@Query(value="UPDATE animals SET a_name=?1, c_lifespan=?2, c_diet=?3 WHERE a_id=?4", nativeQuery = true)
+	public boolean update(String name, double lifespan, String diet, int id);
+	
+	@Query(value="SELECT * FROM animals where a_id=?1", nativeQuery = true)
+	public Animals findById(int id);
+	
+}
