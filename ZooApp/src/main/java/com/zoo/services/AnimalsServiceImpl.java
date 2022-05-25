91d5ec7953039b2389d300b95e3f635a1fb9edfc
jlayestas <93059207+jlayestas@users.noreply.github.com>
2022-05-25 12:24:09 -0600

    AnimalServiceImpl with Logger
    Added logger to this class

--- ZooApp/src/main/java/com/zoo/services/AnimalsServiceImpl.java
+++ ZooApp/src/main/java/com/zoo/services/AnimalsServiceImpl.java
@@ -2,6 +2,7 @@ package com.revature.services;
 
 import java.util.List;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,8 +17,12 @@ public class AnimalsServiceImpl implements AnimalsService {
 	@Autowired
 	private AnimalsRepository arepo;
 	
+	private static Logger log = Logger.getLogger(AnimalsServiceImpl.class);
+	
 	@Override
 	public boolean createAnimal(Animals animals) {
+		log.info("In service layer, creating a new animal: " + animals);
+		
 		int pk = arepo.save(animals).getId();
 		return (pk > 0) ? true : false;
 		
@@ -25,6 +30,9 @@ public class AnimalsServiceImpl implements AnimalsService {
 
 	@Override
 	public Animals getAnimalsById(int id) {;
+	
+	log.info("In service layer, getting animal by id:" + id);
+	
 				System.out.println("ID: " + id);
 				//return crepo.findById(id).stream().findFirst().get();
 				return arepo.findById(id);
@@ -33,11 +41,17 @@ public class AnimalsServiceImpl implements AnimalsService {
 
 	@Override
 	public List<Animals> getAllAnimals() {
+		
+		log.info("In service layer, getting list of all Animals");
+		
 		return arepo.findAll();
 	}
 
 	@Override
 	public boolean updateAnimals(Animals animals) {
+		
+		log.info("In service layer, updating an animal: " + animals);
+		
 		Animals target = arepo.findById(animals.getId());
 		target.setName(animals.getName());
 		target.setLifespan(animals.getLifespan());
@@ -47,6 +61,9 @@ public class AnimalsServiceImpl implements AnimalsService {
 
 	@Override
 	public boolean deleteAnimals(Animals animals) {
+		
+		log.info("In service layer, deleting an animal: " + animals);
+		
 		arepo.delete(animals);
 		return true;
 	}
