50e2b7be182a71c73d312f3f40058689bf8cca4e
Enock Lee <EnochLeee@gmail.com>
2022-06-03 16:33:39 -0500

    minor details change

--- ZooApp/src/main/java/com/zoo/repositories/UserRepository.java
+++ ZooApp/src/main/java/com/zoo/repositories/UserRepository.java
@@ -13,13 +13,13 @@ import com.zoo.models.User;
 @Transactional
 public interface UserRepository extends JpaRepository<User, Integer>{
 	
-	@Query(value="UPDATE user SET u_first_name=?1, u_last_name=?2, u_email=?3, u_password=?4 WHERE u_id=?5", nativeQuery=true)
+	@Query(value="UPDATE user SET u_first_name=?1, u_last_name=?2, u_email=?3, u_password=?4 WHERE u_id=?5, RETURNING true", nativeQuery=true)
 	public boolean update(String firstName, String lastName, String email, String password, int id);
 	
-	@Query(value="SELECT * FROM username WHERE u_id=?1", nativeQuery=true)
+	@Query(value="SELECT * FROM users WHERE u_id=?1", nativeQuery=true)
 	public User findById(int id);
 	
-	@Query(value="SELECT * FROM user WHERE u_username=?1, u_password=?2", nativeQuery=true)
+	@Query(value="SELECT * FROM users WHERE u_username=?1 AND u_password=?2", nativeQuery=true)
 	public User userLogin (String username, String password);
 	
 	@Query(value="SELECT * FROM animal name WHERE a_id=?1", nativeQuery=true)
